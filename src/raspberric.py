934619deb34ddc49d960865cadeeb146233f5c29
amallecourt <adrien.mallecourt@gmail.com>
2015-03-09 19:01:14 +0100

    adress changement

--- src/raspberric.py
+++ src/raspberric.py
@@ -1,2 +1,11 @@
+LOCAL = 0
+
+def correctUrl():
+	if LOCAL == 1 :
+		url = ""
+	else 
+		url = "http://raspberricdevgone.ddns.net/"
+	return url
+
 def getData():
     return "This the raspberric data"
