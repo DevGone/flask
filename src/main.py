dd356ceb1c15712430588005c8a00476b8c4adbd
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-09 18:46:40 +0100

    templates

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from raspberric import getData
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return getData()
+    return "Yo bitch !"
+
+@app.route("/getdata/")
+@app.route("/getdata/<name>")
+def test(name=None):
+    return render_template('hello.html', name=name, data=getData())
 
 if __name__ == "__main__":
     app.run()
