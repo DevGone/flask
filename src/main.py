b4d8d8afb74511497b9044bfdabc5e1e269d8cea
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-13 11:14:54 +0100

    tried polling tests

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
-from raspberric import getData
+from raspberric import getData, polling
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "Yo bitch !"
+	return "Yo bitch !"
 
 @app.route("/getdata/")
 @app.route("/getdata/<name>")
 def test(name=None):
-    return render_template('hello.html', name=name, data=getData())
+	return render_template('hello.html', name=name, data=getData())
+
+def bgr():
+	print "coucou"
 
 if __name__ == "__main__":
-    app.run()
+	polling(1, bgr)
+	app.run()
--- src/raspberric.py
+++ src/raspberric.py
@@ -1,2 +1,17 @@
+import threading
+
+LOCAL = 0
+
+def correctUrl():
+	if LOCAL == 1 :
+		url = ""
+	else:
+		url = "http://raspberricdevgone.ddns.net/"
+	return url
+
 def getData():
     return "This the raspberric data"
+
+def polling(timeInterval, callback):
+  threading.Timer(timeInterval, polling).start()
+  callback()
