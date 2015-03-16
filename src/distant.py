16dec8f7301c02c36017a81792de84ac434ba91f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-16 10:48:45 +0100

    added more logs

--- src/distant.py
+++ src/distant.py
@@ -11,9 +11,10 @@ def polling(timeInterval):
 		i+=1
 
 def fetchRaspberricData(measureId):
-	print 'Fetching data from raspberric'
+	print 'Fetching data from raspberric...'
 	raspberricId = getRaspberricId();
 	measure = json.loads(get_history("papp", 60*60, 'hour', 24))
+	print 'Data fetched'
 	data = json.dumps({'measure_id': measureId, 'raspberric_id': raspberricId, 'measure': measure})
 	return data
 
@@ -24,12 +25,13 @@ def sendData(data, url="http://devgone.herokuapp.com/measures/"):
 
 def repeatTask(measureId):
 	json = fetchRaspberricData(measureId)
-	print sendData(json)
+	sendData(json)
+	print 'Data sent'
 
 def startPollingRaspberric(timeInterval):
 	if "polling" not in startPollingRaspberric.__dict__:
+		startPollingRaspberric.polling = True
 		print 'Start polling'
 		thread = threading.Thread(target=polling, name='Polling', kwargs={'timeInterval': timeInterval})
 		thread.daemon = True
 		thread.start()
-		startPollingRaspberric.polling = True
